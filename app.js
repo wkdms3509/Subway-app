8385bd02de3e3e544a91d3bddd50c4339d153b25
ella <ella@choeja-eun-ui-MacBookAir.local>
2022-01-12 23:02:51 +0900

    add showMenu function

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 "use strict";
 exports.__esModule = true;
-var menuList_js_1 = require("./menuList.js");
-var a = menuList_js_1.menuList;
-console.log(a);
+var App = /** @class */ (function () {
+    function App(isTogo) {
+        this.receipe = [];
+        this.totalPrice = 0;
+        this._isTogo = isTogo;
+        this._hasVaccinePass = false;
+    }
+    App.prototype.showMenu = function () {
+        console.log('------menu------');
+        console.log('1. 이탈리안 비엠티');
+        console.log('2. 이탈리안 비엠티 에그마요');
+        console.log('3. 비엘티');
+        console.log('4. 미트볼');
+        console.log('5. 터키');
+        console.log('6. 베이컨 에그');
+        console.log('7. 로스트 치킨');
+        console.log('8. 베지');
+        console.log('----------------');
+    };
+    return App;
+}());
+var a = new App(true);
+console.log(a.showMenu());
--- app.ts
+++ app.ts
@@ -6,12 +6,28 @@ class App {
     private _isTogo: boolean;
     private _hasVaccinePass: boolean;
 
-    constructor() {
+    constructor(isTogo: boolean) {
         this.receipe = [];
         this.totalPrice = 0;
-        this._isTogo = false;
+        this._isTogo = isTogo;
         this._hasVaccinePass = false;
     }
 
-    
+    showMenu() {
+        console.log('------menu------');
+        console.log('1. 이탈리안 비엠티');
+        console.log('2. 이탈리안 비엠티 에그마요');
+        console.log('3. 비엘티');
+        console.log('4. 미트볼');
+        console.log('5. 터키');
+        console.log('6. 베이컨 에그');
+        console.log('7. 로스트 치킨');
+        console.log('8. 베지');
+        console.log('----------------');
+    }
+
 }
+
+const a = new App(true);
+
+console.log(a.showMenu());
