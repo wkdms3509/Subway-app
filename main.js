6ac638abcb020e6fca63ca536946d892bff3fe0f
ella <ella@choeja-eun-ui-MacBookAir.local>
2022-01-21 21:26:19 +0900

    create main.ts file

---
+++ main.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+var app_js_1 = require("./app.js");
+var app = new app_js_1.App();
+while (app.totalPrice === 0) {
+    app.init(1);
+    app.init(2);
+    app.init(3);
+    app.init(4);
+    app.init(5);
+    app.init(6);
+    app.init(7);
+    app.init(8);
+    app.init(9);
+    app.init(10);
+    app.init(11);
+}
---
+++ main.ts
@@ -0,0 +1,18 @@
+import { App } from './app.js';
+
+const app = new App();
+
+while(app.totalPrice === 0) {
+    app.init(1);
+    app.init(2);
+    app.init(3);
+    app.init(4);
+    app.init(5);
+    app.init(6);
+    app.init(7);
+    app.init(8);
+    app.init(9);
+    app.init(10);
+    app.init(11);
+}
+
